e70fa1f9d2d3525abb0ad97f4325fc0cb412c5f7
Sun Daowen <sundaowen@gmail.com>
2020-03-18 17:08:27 +0800

    2020 build on linux linuxmint 19.3 gcc 7.4.0

---
+++ curl/make_linux_x86_32.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pushd "`dirname "$0"`"
+rootdir=`pwd`
+tmpdir=/tmp/libsundaowen_curl_with_openssl
+target=linux_x86_32
+prefix=$tmpdir/$target
+version=`cat version.txt`
+rm -rf "$tmpdir/$version"
+mkdir -p "$tmpdir/$version"
+cp -rf "../$version/"* "$tmpdir/$version"
+pushd "$tmpdir/$version"
+rm -rf build
+mkdir build
+cd build
+cmake -DBUILD64=OFF -C "$rootdir/CMakeLists.txt" -DBUILD_CURL_EXE=OFF -DBUILD_SHARED_LIBS=OFF -DOPENSSL_INCLUDE_DIR="$rootdir/../openssl/include/$target" -DOPENSSL_CRYPTO_LIBRARY="$rootdir/../openssl/lib/$target/libcrypto.a" -DOPENSSL_SSL_LIBRARY="$rootdir/../openssl/lib/$target/libssl.a" -DCURL_ZLIB=OFF -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="$prefix" ..
+cmake --build . --target install --config Release --clean-first
+popd
+mkdir -p "../target/include/$target"
+cp -rf "$prefix/include/"* "../target/include/$target"
+mkdir -p "../target/lib/$target"
+cp -f "$prefix/lib/"*.a "../target/lib/$target"
+popd
+rm -rf "$tmpdir"
